1ff59dfb724d26a8810f14e3bc6c5b483a2ea0dd
Qweeqer <103505333+Qweeqer@users.noreply.github.com>
2022-07-10 19:35:57 +0000

    Deploying to gh-pages from @ Qweeqer/goit-js-hw-09@cf77edda12ddf362348274686617c61e4938c37d 🚀

---
+++ 01-color-switcher.a562dfb7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;console.log(n),t.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.a562dfb7.js.map
